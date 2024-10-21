b0b9747c14809c920bbaa3bed57d1156c8add61a
faey69 <faeych@gmail.com>
2024-10-21 17:20:52 +0800

    Add files via upload

--- js/navSetup.js
+++ js/navSetup.js
@@ -1,6 +1,15 @@
 const burgerBtn = document.getElementById('burgerBtn');
 const sideNav = document.getElementById('sideNav');
+const mainContentDiv = document.getElementById('mainContentDiv');
 
+// Toggle sidebar when clicking the burger button
 burgerBtn.addEventListener('click', () => {
   sideNav.classList.toggle('-translate-x-full');
+});
+
+// Hide sidebar when clicking/tapping on main content
+mainContentDiv.addEventListener('click', () => {
+  if (!sideNav.classList.contains('-translate-x-full')) {
+    sideNav.classList.add('-translate-x-full');
+  }
 });
